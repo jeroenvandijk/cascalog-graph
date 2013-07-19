e87935f2b152322835f88fd12e3b45ccfbf23778
Jeroen van Dijk <jeroentjevandijk@gmail.com>
2013-07-19 15:11:19 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject adgoji/cascalog-graph "0.2.5-SNAPSHOT"
+(defproject adgoji/cascalog-graph "0.2.5"
   :description "Graph implementation for Cascalog"
   :url "http://github.com/jeroenvandijk/cascalog-graph"
   :license { :name "Eclipse Public License"
